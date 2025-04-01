419bcb4b3fe0c596597f046cd9d0679f62a8f3bd
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2025-04-01 12:07:08 +0000

    adiciona arquivos iniciais do projeto, incluindo .nvmrc, package.json e página inicial

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <div>
+      <h1>Esse foi o seu primeiro passo</h1>
+      <h3>
+        Sei que não foi fácil chegar até aqui. Você enfrentou diversas barreiras
+        sozinho, mas provou ser capaz. Por isso, não desista!
+      </h3>
+
+      <p>
+        Você já deu o primeiro passo, e agora é hora de continuar avançando. A
+        jornada pode ser desafiadora, mas lembre-se de que cada passo conta.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
